bf798ec95a8956ddd558dd1592514003f0d83d91
SSRayane <r.serir@spinalcom.com>
2025-05-30 18:57:42 +0200

    near

--- src/processes/status/SyncRunHandler/SyncRunPull.ts
+++ src/processes/status/SyncRunHandler/SyncRunPull.ts
@@ -82,6 +82,13 @@ export class SyncRunPull {
   equipmentGroup: SpinalNode<any>;
   mappingCodeToEquipment: Map<string, SpinalNode<any>>;
   buildingNode: SpinalNode<any>;
+  cpFlux4S: SpinalNode<any>;
+  cpBadges4S: SpinalNode<any>;
+  cpFluxLLG: SpinalNode<any>;
+  cpBadgesLLG: SpinalNode<any>;
+  cpFluxOPERA: SpinalNode<any>;
+  cpBadgesOPERA: SpinalNode<any>;
+
 
 
   constructor(
@@ -232,6 +239,62 @@ export class SyncRunPull {
     SpinalGraphService._addNode(buildingNode);
     this.buildingNode = buildingNode;
     console.log('Building Node initialized:', buildingNode.getName().get());
+  }
+
+  async initControlPoints(): Promise<void> {
+    const cpProfiles = await this.buildingNode.getChildren('hasControlPoints');
+    const profile4S = cpProfiles.find((profile) => profile.getName().get() === process.env.PROFIL_CP_4S);
+    const profileLLG = cpProfiles.find((profile) => profile.getName().get() === process.env.PROFIL_CP_LLG);
+    const profileOPERA= cpProfiles.find((profile) => profile.getName().get() === process.env.PROFIL_CP_OPERA);
+    if (!profile4S || !profileLLG || !profileOPERA) {
+      throw new Error('Control Points Profiles Not Found');
+    }
+
+    const controlPoints4S = await profile4S.getChildren('hasBmsEndpoint');
+    const controlPointsLLG = await profileLLG.getChildren('hasBmsEndpoint');
+    const controlPointsOPERA = await profileOPERA.getChildren('hasBmsEndpoint');
+
+    
+    const cpFlux4S = controlPoints4S.find((cp) => cp.getName().get() === process.env.CP_NAME_FLUX_4S);
+    const cpBadges4S = controlPoints4S.find((cp) => cp.getName().get() === process.env.CP_NAME_BADGES_4S);
+
+    const cpFluxLLG = controlPointsLLG.find((cp) => cp.getName().get() === process.env.CP_NAME_FLUX_LLG);
+    const cpBadgesLLG = controlPointsLLG.find((cp) => cp.getName().get() === process.env.CP_NAME_BADGES_LLG);
+
+    const cpFluxOPERA = controlPointsOPERA.find((cp) => cp.getName().get() === process.env.CP_NAME_FLUX_OPERA);
+    const cpBadgesOPERA = controlPointsOPERA.find((cp) => cp.getName().get() === process.env.CP_NAME_BADGES_OPERA);
+
+
+    if (!cpFlux4S || !cpBadges4S || !cpFluxOPERA || !cpBadgesOPERA || !cpFluxLLG || !cpBadgesLLG) {
+      console.error('Control Points not found : ', {
+        cpFlux4S: cpFlux4S ? cpFlux4S.getName().get() : 'Not Found',
+        cpBadges4S: cpBadges4S ? cpBadges4S.getName().get() : 'Not Found',
+        cpFluxOPERA: cpFluxOPERA ? cpFluxOPERA.getName().get() : 'Not Found',
+        cpBadgesOPERA: cpBadgesOPERA ? cpBadgesOPERA.getName().get() : 'Not Found',
+        cpFluxLLG: cpFluxLLG ? cpFluxLLG.getName().get() : 'Not Found',
+        cpBadgesLLG: cpBadgesLLG ? cpBadgesLLG.getName().get() : 'Not Found',
+      });
+      throw new Error('Control Points Not Found');
+    }
+    SpinalGraphService._addNode(cpFlux4S);
+    SpinalGraphService._addNode(cpBadges4S);
+    SpinalGraphService._addNode(cpFluxOPERA);
+    SpinalGraphService._addNode(cpBadgesOPERA);
+    SpinalGraphService._addNode(cpFluxLLG);
+    SpinalGraphService._addNode(cpBadgesLLG);
+
+    this.cpFlux4S = cpFlux4S;
+    this.cpBadges4S = cpBadges4S;
+    this.cpFluxLLG = cpFluxLLG;
+    this.cpBadgesLLG = cpBadgesLLG;
+    this.cpFluxOPERA = cpFluxOPERA;
+    this.cpBadgesOPERA = cpBadgesOPERA;
+
+    console.log('Control Points initialized:', {
+      '4S': this.cpFlux4S.getName().get() + ' and ' + this.cpBadges4S.getName().get(),
+      cpLLG: this.cpFluxLLG.getName().get() + ' and ' + this.cpBadgesLLG.getName().get(),
+      cpOPERA: this.cpFluxOPERA.getName().get() + ' and ' + this.cpBadgesOPERA.getName().get(),
+    });
   }
 
   async initNetworkNodes(): Promise<void> {
@@ -694,14 +757,31 @@ export class SyncRunPull {
 
   async calculateOccupants(): Promise<void> {
     //In this function, we try to calculate the number of occupants in the building in different ways
-    // 1-  Add and substract every in and out of every device
+    // 1-  Add and substract in and outs of specific devices for each building
     // 2-  Count the number of badges scanned daily
 
     // 1 :
+    const deviceNumbersOPERA = process.env.LBE_OPERA.split(',');
+    const deviceNumbersLLG = process.env.LBE_LLG.split(',');
+    const deviceNumbers4S = process.env.LBE_4S.split(',');
+    console.log(
+      'Device Numbers for OPERA:',
+      deviceNumbersOPERA,
+      'LLG:',
+      deviceNumbersLLG,
+      '4S:',
+      deviceNumbers4S
+    );
     const deviceNodes: SpinalNode<any>[] =
       await this.virtualNetworkContext.getChildren('hasBmsDevice');
+
+    let totalOccupantsOPERA = 0;
+    let totalOccupantsLLG = 0;
+    let totalOccupants4S = 0;
     let totalOccupants = 0;
+
     for (const deviceNode of deviceNodes) {
+      const deviceName = deviceNode.getName().get();
       const endpoints = await deviceNode.getChildren('hasBmsEndpoint');
       const inEndpoint = endpoints.find(
         (endpoint) => endpoint.getName().get() === 'In'
@@ -718,19 +798,73 @@ export class SyncRunPull {
       }
       const inElement = await inEndpoint.element.load();
       const outElement = await outEndpoint.element.load();
+      const inValue = inElement.currentValue.get();
+      const outValue = outElement.currentValue.get();
       totalOccupants +=
-        inElement.currentValue.get() - outElement.currentValue.get();
+      inValue - outValue;
+
+     
+      if( deviceNumbersOPERA.some((deviceNumber) => deviceName.includes(deviceNumber))){
+        console.log(
+          `Device OPERA: ${deviceName}, In: ${inValue}, Out: ${outValue}, Difference: ${inValue - outValue}`
+        );
+        totalOccupantsOPERA += inValue - outValue;
+      }
+      else if( deviceNumbersLLG.some((deviceNumber) => deviceName.includes(deviceNumber))){
+        console.log(
+          `Device LLG: ${deviceName}, In: ${inValue}, Out: ${outValue}, Difference: ${inValue - outValue}`
+        );
+        totalOccupantsLLG += inValue - outValue;
+      }
+      else if( deviceNumbers4S.some((deviceNumber) => deviceName.includes(deviceNumber))){
+        console.log(
+          `Device 4S: ${deviceName}, In: ${inValue}, Out: ${outValue}, Difference: ${inValue - outValue}`
+        );
+        totalOccupants4S += inValue - outValue;
+      }
     }
 
     console.log(
-      'Total occupants calculated from In/Out endpoints:',
-      totalOccupants
+      'Occupants calculated from In/Out endpoints:',
+     {'OPERA': totalOccupantsOPERA, 'LLG': totalOccupantsLLG, '4S': totalOccupants4S, 'Total': totalOccupants}
     );
+
+    const cpFluxOperaModel = await this.cpFluxOPERA.element.load();
+    const cpBadgesOperaModel = await this.cpBadgesOPERA.element.load();
+    const cpFluxLLGModel = await this.cpFluxLLG.element.load();
+    const cpBadgesLLGModel = await this.cpBadgesLLG.element.load();
+    const cpFlux4SModel = await this.cpFlux4S.element.load();
+    const cpBadges4SModel = await this.cpBadges4S.element.load();
+
+    cpFluxOperaModel.currentValue.set(totalOccupantsOPERA);
+    cpFluxLLGModel.currentValue.set(totalOccupantsLLG);
+    cpFlux4SModel.currentValue.set(totalOccupants4S);
+    
+    await this.timeseriesService.pushFromEndpoint(
+      this.cpFluxOPERA.info.id.get(),
+      totalOccupantsOPERA,
+    );
+    await this.timeseriesService.pushFromEndpoint(
+      this.cpFluxLLG.info.id.get(),
+      totalOccupantsLLG,
+    );
+    await this.timeseriesService.pushFromEndpoint(
+      this.cpFlux4S.info.id.get(),
+      totalOccupants4S,
+    );
+    console.log(
+      'Control Points updated with total occupants from In/Out endpoints'
+    );
+
     // 2 :
     const occupantNodes = await spinalOccupantService.getOccupants(
       this.occupantContext.getName().get()
     );
     const totalOccupantsFromBadges = occupantNodes.length;
+
+    cpBadgesOperaModel.currentValue.set(totalOccupantsFromBadges);
+    cpBadgesLLGModel.currentValue.set(totalOccupantsFromBadges);
+    cpBadges4SModel.currentValue.set(totalOccupantsFromBadges);
 
     console.log(
       'Total occupants calculated from badges:',
@@ -758,6 +892,7 @@ export class SyncRunPull {
     }
     console.log('Endpoint values reset to 0 successfully');
     console.log('Cleaning known daily occupants...');
+
     await spinalOccupantService.deleteAllOccupants(
       this.occupantContext.getName().get()
     );
@@ -774,6 +909,8 @@ export class SyncRunPull {
       await this.initEquipmentGroup(); // for tickets and bmsdevices link 
       await this.initTicketNodes();
       await this.initBuildingNode();
+      await this.initControlPoints();
+
       this.occupantContext = await spinalOccupantService.createOrGetContext(process.env.OCCUPANT_CONTEXT_NAME);
       this.organizationContext = await spinalOrganizationService.createOrGetContext(process.env.ORGANIZATION_CONTEXT_NAME);
       await this.initNetworkNodes();
